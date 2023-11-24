fdafab683d0073115202542290444660bb772013
Otavio Dantas <otavio.dantas@federalconsig.com.br>
2023-11-24 15:18:16 -0300

    Definindo estrutura de listagem de servidores

--- app/routers/minecraft.py
+++ app/routers/minecraft.py
@@ -1,3 +1,4 @@
+import re
 from fastapi import APIRouter
 import os
 
@@ -11,4 +12,22 @@ router = APIRouter(
 async def list_all_servers():
     cmd = "/usr/local/bin/msm server list"
     result = os.popen(cmd).read().split("\n")
-    return result
+
+    filtered = list(filter(lambda x: x != "", result))
+
+    def format(row):
+        reg_status = "(\[.*\])"
+        reg_server_name = "(\".*\")"
+        reg_message = "(\..*\.)"
+
+        status = re.search(reg_status, row).group()
+        server_name = re.search(reg_server_name, row).group()
+        message = re.search(reg_message, row).group()
+
+        return {
+            "status": status[1:len(status) - 1],
+            "serverName": server_name[1:len(server_name) - 1],
+            "message": message[2:len(message) - 1]
+        }
+
+    return [format(x) for x in filtered]
