ddc8ce34a00953bf746afe538ff83afa9aa8811d
Otavio Dantas <otavio.dantas@federalconsig.com.br>
2023-11-27 23:39:53 -0300

    Refatorando para service

--- app/routers/minecraft.py
+++ app/routers/minecraft.py
@@ -1,6 +1,5 @@
-import re
 from fastapi import APIRouter
-import os
+from app.services.minecraft import Minecraft
 
 router = APIRouter(
     prefix="/minecraft",
@@ -8,26 +7,12 @@ router = APIRouter(
     responses={404: {"description": "Não Encontrado"}},
 )
 
+minecraft = Minecraft()
+
 @router.get("/server/list")
 async def list_all_servers():
-    cmd = "/usr/local/bin/msm server list"
-    result = os.popen(cmd).read().split("\n")
+    return minecraft.list_all_servers()
 
-    filtered = list(filter(lambda x: x != "", result))
-
-    def format(row):
-        reg_status = "(\[.*\])"
-        reg_server_name = "(\".*\")"
-        reg_message = "(\..*\.)"
-
-        status = re.search(reg_status, row).group()
-        server_name = re.search(reg_server_name, row).group()
-        message = re.search(reg_message, row).group()
-
-        return {
-            "status": status[1:len(status) - 1],
-            "serverName": server_name[1:len(server_name) - 1],
-            "message": message[2:len(message) - 1]
-        }
-
-    return [format(x) for x in filtered]
+@router.get("/{server}/start")
+async def start_server(server: str):
+    return minecraft.start_server()
